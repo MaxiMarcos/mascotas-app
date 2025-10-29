6e241871fed4e665f2be4078058ad4dddba7aa30
Maxi <maximilianoabelmarcos@gmail.com>
2025-10-29 16:35:26 -0300

    cambié listen 80; por listen  y añado export PORT=80 en start.sh

--- encontraTuMascota/start.sh
+++ encontraTuMascota/start.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
+set -e
+
+# Asegurar valor por defecto de PORT para nginx
+export PORT="${PORT:-80}"
 
 # Reemplazar el placeholder con la URL real del backend
 if [ -n "$API_URL" ]; then
-    sed -i "s|API_URL_PLACEHOLDER|$API_URL|g" /usr/share/nginx/html/index.html
+  sed -i "s|API_URL_PLACEHOLDER|$API_URL|g" /usr/share/nginx/html/index.html
 fi
 
-# Configurar el puerto de nginx
-envsubst '$$PORT' < /etc/nginx/nginx.conf > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf
+# Sustituir PORT en nginx.conf
+envsubst '${PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 # Iniciar nginx
 nginx -g 'daemon off;'
